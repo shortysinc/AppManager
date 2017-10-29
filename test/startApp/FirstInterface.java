3c6e63318081a4887b244065f319e8073ff8b0e4
Jorge R. <jorge.shortys@gmail.com>
2017-10-29 11:03:05 +0100

    First Git

---
+++ test/startApp/FirstInterface.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package startApp;
+
+/**
+ * @author EXT127
+ *
+ */
+public class FirstInterface {
+
+	/**
+	 * 
+	 */
+	public FirstInterface() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

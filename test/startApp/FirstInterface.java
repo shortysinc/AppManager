7dce28bc4b4ffbda39a9a804be346fb818613d8b
Jorge R. <jorge.shortys@gmail.com>
2017-10-31 21:06:44 +0100

    rolf

--- test/startApp/FirstInterface.java
+++ test/startApp/FirstInterface.java
@@ -14,19 +14,20 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class FirstInterface {
 
 	private JFrame KillProcesses;
-	private JTextField ip;
-	
+	private JTextField ip= new JTextField();
+	JTextArea execution = new JTextArea();
 
 	JComboBox<Dirs> OptionChooser = new JComboBox<Dirs>();
 	
 	
-	private String command="";
+	private String command, ServiceON, ServiceOff="";
 	
 	private Pattern pattern;
     private Matcher matcher;
@@ -42,7 +43,6 @@ public class FirstInterface {
 		OptionChooser = optionChooser;
 	}
 
-	@SuppressWarnings("unused")
 	private boolean ipValidator(final String ip){
 		  matcher = pattern.matcher(ip);
 		  return (matcher.matches() && !ip.equals(""));
@@ -61,7 +61,8 @@ public class FirstInterface {
 	/**
 	 * Create the application.
 	 */
-	public FirstInterface() {
+	public FirstInterface() throws Exception {
+		pattern = Pattern.compile(IPADDRESS_PATTERN);
 		initialize();
 	}
 	
@@ -74,29 +75,66 @@ public class FirstInterface {
 	private void parser(Dirs dir, String ip) {
 		switch (dir) {
 		case IE:
-			this.setCommand("cmd /s "+ip+" /f /im iexplore.exe");
+			this.setCommand("taskkill /s "+ip+" /f /im iexplore.exe");
 			break;
 		case EDGE:
-			this.setCommand("cmd /s "+ip+" /f /im MicrosoftEdge.exe");
+			this.setCommand("taskkill /s "+ip+" /f /im MicrosoftEdge.exe");
 			break;
 			
 		case EXCEL:
-			this.setCommand("cmd /s "+ip+" /f /im excel.exe");
+			this.setCommand("taskkill /s "+ip+" /f /im excel.exe");
 			break;
 		case WORD:
-			this.setCommand("cmd /s "+ip+" /f /im winword.exe");
+			this.setCommand("taskkill /s "+ip+" /f /im winword.exe");
 			break;
 		case ONELOG:
-			this.setCommand("cmd /s "+ip+" /f /im LoginApplication.exe");
+			//this.setCommand("taskkill /s "+ip+" /f /im LoginApplication.exe");
+			this.setServiceOff("sc \\\\"+ip+" stop \"ITS Onelog Client\"");
+			this.setServiceON("sc \\\\"+ip+" start  \"ITS Onelog Client\"");
 			break;
 		case GP:
-			this.setCommand("cmd /s "+ip+" /f /im pangpa.exe");
+			//this.setCommand("taskkill /s "+ip+" /f /im pangpa.exe");
+			//this.setCommand("sc \\\\"+ip+" stop pangps && timeout 5 && sc \\\\"+ip+" start pangps");
+			this.setServiceOff("sc \\\\"+ip+" stop pangps");
+			this.setServiceON("sc \\\\"+ip+" start pangps");
 			break;
 		case CHROME:
-			this.setCommand("cmd /s "+ip+" /f /im chrome.exe");
+			this.setCommand("taskkill /s "+ip+" /f /im chrome.exe");
 			break;
 		default:
 			break;
+		}
+	}
+	
+	private void ExecuteCMD() {
+		String commandRemote = this.getCommand();
+		System.out.println(commandRemote);
+		//String command = "powershell Rename-Item -path " + path.toString() + " -NewName "+ path.toString()+".old" + " -force ";
+		try {
+			Process CMDProcess = Runtime.getRuntime().exec(commandRemote);
+			CMDProcess.getOutputStream().close();
+			execution.append("\nSe ha cerrado correctamente el proceso");
+		}
+		catch (Exception e) {
+			JOptionPane.showMessageDialog(null,"Ha habido un fallo al cerrar el proceso");
+			//e.getStackTrace();
+		}
+	}
+	
+	private void ExecuteService() {
+		String ServiceON = this.getServiceON();
+		String ServiceOff = this.getServiceOff();
+		try {
+			Process SON = Runtime.getRuntime().exec(ServiceON);
+			SON.getOutputStream().close();
+			Thread.sleep(25);
+			Process SOff = Runtime.getRuntime().exec(ServiceOff);
+			SOff.getOutputStream().close();
+			execution.append("\nSe ha reiniciado correctamente el proceso");
+		}
+		catch (Exception e) {
+			JOptionPane.showMessageDialog(null,"Ha habido un fallo al cerrar el proceso");
+			//e.getStackTrace();
 		}
 	}
 
@@ -105,15 +143,18 @@ public class FirstInterface {
 	 */
 	private void initialize() {
 		KillProcesses = new JFrame();
+		KillProcesses.getContentPane().setForeground(Color.LIGHT_GRAY);
+		KillProcesses.getContentPane().setBackground(Color.LIGHT_GRAY);
 		KillProcesses.setForeground(Color.ORANGE);
 		KillProcesses.setTitle("Kill Processes");
 		KillProcesses.setIconImage(Toolkit.getDefaultToolkit().getImage(FirstInterface.class.getResource("/startApp/CGP.png")));
 		KillProcesses.setResizable(false);
-		KillProcesses.setBounds(100, 100, 439, 104);
+		KillProcesses.setBounds(100, 100, 472, 104);
 		KillProcesses.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		KillProcesses.getContentPane().setLayout(null);
 		
 		JLabel IpLabel = new JLabel("IP/Machine Name:");
+		IpLabel.setForeground(Color.BLACK);
 		IpLabel.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		IpLabel.setBounds(10, 11, 98, 14);
 		KillProcesses.getContentPane().add(IpLabel);
@@ -122,28 +163,28 @@ public class FirstInterface {
 		OptionChooser.setToolTipText("\r\n");
 		OptionChooser.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		OptionChooser.setModel(new DefaultComboBoxModel<Dirs>(Dirs.values()));
-		OptionChooser.setBounds(281, 8, 137, 20);
+		OptionChooser.setBounds(317, 8, 137, 20);
 		KillProcesses.getContentPane().add(OptionChooser);
 		
 		
 		ip.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		ip.setBounds(108, 8, 109, 20);
+		ip.setBounds(108, 8, 150, 20);
 		KillProcesses.getContentPane().add(ip);
 		ip.setColumns(10);
 		
 		JLabel lblProcess = new JLabel("Process:");
 		lblProcess.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		lblProcess.setBounds(234, 11, 46, 14);
+		lblProcess.setBounds(270, 11, 46, 14);
 		KillProcesses.getContentPane().add(lblProcess);
 		
-		JTextArea execution = new JTextArea();
+		
 		execution.setFont(new Font("Monospaced", Font.PLAIN, 11));
 		execution.setEditable(false);
-		execution.setBounds(10, 32, 207, 32);
+		execution.setBounds(10, 32, 248, 32);
 		KillProcesses.getContentPane().add(execution);
 		
 		JButton SendButton = new JButton("Send");
-		SendButton.setBounds(234, 40, 89, 23);
+		SendButton.setBounds(270, 40, 89, 23);
 		KillProcesses.getContentPane().add(SendButton);
 		
 		
@@ -151,21 +192,28 @@ public class FirstInterface {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//parser((Dirs) OptionChooser.getSelectedItem(), ip.getText());
-				//execution.setText(command);
+				parser((Dirs) OptionChooser.getSelectedItem(), ip.getText());
+				execution.setText(command);
 				//System.out.println(command);
-				//String ipField=ip.getText();
-				//Dirs option= (Dirs) OptionChooser.getSelectedItem();
-				/*if(ipValidator(ipField)) {
-					parser(option, ipField);
+				String ipField=ip.getText().replaceAll("\\s+", "");
+				Dirs option= (Dirs) OptionChooser.getSelectedItem();
+				if(ipValidator(ipField)) {
+					if(getCommand().contains("explorer")||getCommand().contains("pangp")) {
+						parser(option, ipField);
+						ExecuteService();
+					}
+					else {
+						parser(option, ipField);
+						ExecuteCMD();
+					}
 				}
 				else {
-					execution.setText("La IP introducida no es correcta...");
-				}*/
+					execution.setText("La IP introducida no es \ncorrecta...");
+				
 			}
 			
 			
-		});
+		}});
 		
 		JButton CancelButton = new JButton("Cancel");
 		CancelButton.addActionListener(new ActionListener() {
@@ -173,7 +221,7 @@ public class FirstInterface {
 				System.exit(0);
 			}
 		});
-		CancelButton.setBounds(329, 40, 89, 23);
+		CancelButton.setBounds(365, 40, 89, 23);
 		KillProcesses.getContentPane().add(CancelButton);
 	}
 	
@@ -227,18 +275,34 @@ public class FirstInterface {
 	public void setPattern(Pattern pattern) {
 		this.pattern = pattern;
 	}
-	
+
 	/**
-	 * @return the ip
+	 * @return the serviceON
 	 */
-	public JTextField getIp() {
-		return ip;
+	public String getServiceON() {
+		return ServiceON;
 	}
 
 	/**
-	 * @param ip the ip to set
+	 * @param serviceON the serviceON to set
 	 */
-	public void setIp(JTextField ip) {
-		this.ip = ip;
+	public void setServiceON(String serviceON) {
+		ServiceON = serviceON;
 	}
+
+	/**
+	 * @return the serviceOff
+	 */
+	public String getServiceOff() {
+		return ServiceOff;
+	}
+
+	/**
+	 * @param serviceOff the serviceOff to set
+	 */
+	public void setServiceOff(String serviceOff) {
+		ServiceOff = serviceOff;
+	}
+	
+	
 }
